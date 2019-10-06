33ba8211dba3fb521728db4f19a4ff64fe300846
Masayoshi Kataoka <mkataoka73@gmail.com>
2019-10-06 22:57:49 +0900

    Finish the JavaScript gallery

--- _site/js/gallery.js
+++ _site/js/gallery.js
@@ -1,3 +1,26 @@
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo img");
+  thumbnails.forEach( function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as main image.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      let newImageAlt = thumbnail.dataset.title;
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", newImageAlt);
+
+      // Change wchich image is current.
+      let currentClass = "current";
+      document.querySelector(".current").classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+
+      // Update gellery informations
+
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+      title.innerHTML      = thumbnail.dataset.title;
+      description.innerHTML= thumbnail.dataset.description;
+    });
+  });
 }
